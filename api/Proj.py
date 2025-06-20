130990f317434c5f25fa57eb459927cb2597da68
HeDashHe13 <Suhaan.f.shaik@gmail.com>
2025-06-20 11:01:49 +0530

    Update Proj.py

--- api/Proj.py
+++ api/Proj.py
@@ -8,8 +8,24 @@ import transformers
 API_KEY="6e14d133-9595-4b9f-85f4-9b67304163e3"
 from dotenv import load_dotenv
 import os
-
+YOUR_COINBASE_PASSPHRASE = HeDashHe13!
+YOUR_COINBASE_SECRET = N/A
 load_dotenv()  # Load variables from .env into the environment
 
 api_key = os.getenv("API_KEY")
 print(f"My API Key is: {API_KEY}")
+# ------------------ COINBASE PRO ------------------
+coinbase = ccxt.coinbasepro({
+    'apiKey': 'YOUR_COINBASE_API_KEY',
+    'secret': 'YOUR_COINBASE_SECRET ',
+    'password': 'YOUR_COINBASE_PASSPHRASE',
+    'enableRateLimit': True,
+})
+
+# Test Coinbase connection
+try:
+    balance = coinbase.fetch_balance()
+    print("✅ Coinbase connected!")
+    print("Balance:", balance['total'])
+except Exception as e:
+    print("❌ Coinbase error:", str(e))
